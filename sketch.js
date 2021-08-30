676213567abac93bc05918b0434ccca85b74c0e6
arshita2 <88212626+arshita2@users.noreply.github.com>
2021-08-30 16:36:36 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var x 
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  x = createSprite(200, 200, 20, 30)
+
+
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+}
+
+
+
+
